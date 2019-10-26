6d05570c779d2c45358cbbf2b6371496dacc1aff
hocineismail <hocineismail75@gmail.com>
2019-10-26 07:50:18 +0200

    Added authenticatio

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
+import Dashboard from './dashboard/dashboard';
+import authProvider from './authProvider/authProvider';
+
 import UserList from './data/users'
 import { PostList, PostEdit, PostCreate }  from './data/posts'
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
   
 function App() {
  
   return (
     <div className="App">
-       <Admin dataProvider={dataProvider}>
-          <Resource name="users" list={UserList}   />
-          <Resource name="posts" list={PostList}  edit={PostEdit}  create={PostCreate}  />         
+       <Admin dashboard={Dashboard}  
+              authProvider={authProvider} 
+              dataProvider={dataProvider}>
+
+          <Resource name="users" 
+                    list={UserList}  
+                    icon={UserIcon} />
+          <Resource 
+                    name="posts" 
+                    list={PostList}  
+                    edit={PostEdit} 
+                    create={PostCreate} 
+                    icon={PostIcon}  />         
        </Admin>
     </div>
   );
