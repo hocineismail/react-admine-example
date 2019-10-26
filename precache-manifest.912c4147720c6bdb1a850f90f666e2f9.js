f2eab1d581c4fc912218c1a1b7983cef43a20612
hocineismail <hocineismail75@gmail.com>
2019-10-26 08:10:31 +0200

    Updates

---
+++ precache-manifest.912c4147720c6bdb1a850f90f666e2f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31463c59c7763e95e11dcf8c3d0fd25",
+    "url": "/react-admine-example/index.html"
+  },
+  {
+    "revision": "069fac05f32042dd06a5",
+    "url": "/react-admine-example/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a8b0adef0f6b8f020c52",
+    "url": "/react-admine-example/static/js/2.6e7d576b.chunk.js"
+  },
+  {
+    "revision": "069fac05f32042dd06a5",
+    "url": "/react-admine-example/static/js/main.29706c60.chunk.js"
+  },
+  {
+    "revision": "69f1eb76e2fe1c4cedca",
+    "url": "/react-admine-example/static/js/runtime-main.020249c2.js"
+  }
+]);
